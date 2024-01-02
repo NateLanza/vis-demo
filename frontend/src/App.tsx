88284a46c0e592a67b4083c9dcaf9bd181dc72ec
Nate Lanza <dolphinpower900@gmail.com>
2024-01-01 21:05:51 -0700

    Basic table rendering complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
+// URL to fetch data from
+const API_URL: string = "/api";
+
+// Default attributes to display
+// Preffered_Position is a typo, just not mine- see soccer_small.json
+const DEFAULT_ATTS: string[] = 
+  ["Name", "Nationality", "Club", "Age", "Preffered_Position"];
+  
 function App() {
+  // State to track whether data has been fetched
+  const [dataLoaded, setDataLoaded] = useState(false);
+  // State to contain data
+  const [data, setData] = useState([]);
+
+  // Fetching data from backend
+  useEffect(() => {
+    // Prevent infinite loop
+    if (dataLoaded) return;
+
+    fetch(API_URL).then(
+      (res: Response) => res.json()
+    ).then(
+      (data: React.SetStateAction<never[]>) => {
+        setData(data);
+        setDataLoaded(true);
+      }
+    );
+  }, [dataLoaded]); // Dependency array
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        {!dataLoaded ? ( // If not loaded, <h4>, else table
+          <h4>Loading player data...</h4>
+        ) : (
+          <table className="data">
+            <thead>
+              <tr>
+                {DEFAULT_ATTS.map((att) => (
+                  <th>{att}</th>
+                ))}
+              </tr>
+            </thead>
+          <tbody>
+            {data.map((player: any) => (
+              <tr>
+                {DEFAULT_ATTS.map((att) => (
+                  <td>{player[att]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        )}
+      </section>
     </div>
   );
 }
